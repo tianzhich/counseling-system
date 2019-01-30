434237c55547e0230a3f5c8b3b3874c151b90b05
tianzhi <tianzhi@kuaishou.com>
2019-01-30 16:00:26 +0800

    conf: cors

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,8 +1,17 @@
 package utils
 
+import "net/http"
+
 // CheckErr to check err like db, file operation
 func CheckErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+// AllowCors to config the CORS
+func AllowCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Add("Access-Control-Allow-Headers", "Content-Type")
+	(*w).Header().Set("Content-Type", "application/json")
 }
