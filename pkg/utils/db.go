6e714832eed6d988b5e2727ced3852526e8ab3ac
tianzhi <tianzhi@kuaishou.com>
2019-02-20 14:23:08 +0800

    refactor: 隐藏dataSourceName

--- pkg/utils/db.go
+++ pkg/utils/db.go
@@ -1,17 +1,3 @@
 package utils
 
-import (
-	"database/sql"
-
-	_ "github.com/go-sql-driver/mysql" // mysql driver
-)
-
-const dsn = "tianzhi:tianzhi@tcp(47.94.223.143:3306)/pcs"
-
-// InitialDb initail the db connection and return the db instance
-func InitialDb() *sql.DB {
-	db, err := sql.Open("mysql", dsn)
-	CheckErr(err)
-
-	return db
-}
+const dataSourceName = "tianzhi:tianzhi@tcp(47.94.223.143:3306)/pcs"
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,6 +1,11 @@
 package utils
 
-import "net/http"
+import (
+	"database/sql"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql" // mysql driver
+)
 
 // CheckErr to check err like db, file operation
 func CheckErr(err error) {
@@ -14,4 +19,12 @@ func AllowCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Add("Access-Control-Allow-Headers", "Content-Type")
 	(*w).Header().Set("Content-Type", "application/json")
+}
+
+// InitialDb initail the db connection and return the db instance
+func InitialDb() *sql.DB {
+	mydbcon := dataSourceName
+	db, err := sql.Open("mysql", mydbcon)
+	CheckErr(err)
+	return db
 }
