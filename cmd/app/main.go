026ef4d8e518da2810b18e2332f60d265ad4d423
tianzhi <tianzhi@kuaishou.com>
2019-02-20 14:21:01 +0800

    refactor: separate handler

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"counseling-system/pkg/feature1"
-	"counseling-system/pkg/register"
+	"counseling-system/pkg/oauth"
 	"log"
 	"net/http"
 )
@@ -10,9 +10,14 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/fake", feature1.Handler)
-	mux.HandleFunc("/api/register", register.Handler)
+
+	oauthHandler(mux)
 
 	log.Println("Listening on port 8081 ...")
 	err := http.ListenAndServe(":8081", mux)
 	log.Fatal(err)
+}
+
+func oauthHandler(mux *http.ServeMux) {
+	mux.HandleFunc("/api/signup", oauth.SignupHandler)
 }
