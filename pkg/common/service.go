930191f663598022589b6b1377f79876ad143ce3
tianzhich <tianzhi@kuaishou.com>
2019-04-04 15:29:22 +0800

    fix

--- pkg/common/service.go
+++ pkg/common/service.go
@@ -56,7 +56,7 @@ func IsUserLogin(r *http.Request) (int, int) {
 func AddNotification(uid int, no Notification) {
 	var insertStr = fmt.Sprintf("insert notification set u_id=%v, type=?, title=?, `desc`=?", uid)
 
-	if _, success := utils.InsertDB(insertStr, no.UID, no.Type, no.Title, no.Desc); !success {
+	if _, success := utils.InsertDB(insertStr, no.Type, no.Title, no.Desc); !success {
 		fmt.Println("新增通知失败！")
 	}
 }
